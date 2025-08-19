0f9b6a8830957cfb7f2c87104b5aab2e9634011b
akshayayadav8255@gmail.com <windows@DESKTOP-L8RNGBN.localdomain>
2025-08-19 02:10:56 -0700

    complr express setup

---
+++ server/app.js
@@ -0,0 +1,27 @@
+// app.js
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+;
+
+/**
+ * 1. Simple root route
+ */
+app.get("/", (req, res) => {
+  res.send("Welcome to Express ES6 Setup 🚀");
+});
+
+/**
+ * 2. Example GET route with plain response
+ */
+app.get("/get", (req, res) => {
+  res.json({ message: "This is a GET route example" });
+});
+
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
